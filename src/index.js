fca1dbb54108bbceea6687686f7327336ed31e5f
Liul-Abera <liulaber43@gmail.com>
2024-09-14 08:55:33 -0400

    personal-card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+const Me = [
+  {
+    photoname: "./asset/le.jpg",
+    name: "Leul Abera ",
+    aboutMe:
+      "I'm a Software Engineer and Full Stack Web Developer. In my free timeenjoy playing chess, singing, and playing the piano.",
+    HTML: "HTMl",
+    CSS: "Css",
+    JS: "Js",
+    web: "web Desing",
+    git: "git and GitHub",
+    React: "React",
+    servelte: "servelt",
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro
+          name="Leul Abera "
+          aboutMe="I'm a Software Engineer and Full Stack Web Developer. In my free timeenjoy playing chess, singing, and playing the piano"
+        />
+        <SkillList
+          HTML="HTMl"
+          CSS="Css"
+          JS="Js"
+          web="web Desing"
+          git="git and GitHub"
+          React="React"
+          serviette="servelt"
+        />
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return <img className="avatar" src="./asset/le.jpg" />;
+}
+function Intro(props) {
+  return (
+    <div>
+      <h1 className=""> {props.name} </h1>
+      <p>
+        I'm a Software Engineer and Full Stack Web Developer. In my free time, I
+        enjoy playing chess, singing, and playing the piano.
+      </p>
+    </div>
+  );
+}
+function SkillList(props) {
+  return (
+    <ul className="skill-list">
+      <li id="html" className="skill">
+        {props.HTML} 👍
+      </li>
+      <li id="js" className="skill">
+        {props.JS}❤️
+      </li>
+      <li id="web" className="skill">
+        {props.web} 👌
+      </li>
+      <li id="git" className="skill">
+        {props.git} 👍
+      </li>
+      <li id="react" className="skill">
+        {props.React} 👍
+      </li>
+      <li id="se" className="skill">
+        {props.SkillList}😒
+      </li>
+    </ul>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
